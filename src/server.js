9d63e06bdf32fbb10dccbc2daa1083abb779b660
YcleptJohn <Ycleptjohn@gmail.com>
2018-03-14 21:50:02 +0000

    Listen and verify githooks

--- src/server.js
+++ src/server.js
@@ -1,26 +1,21 @@
 const express = require('express')
-const tokens = require('../../shared/tokens.json')
-console.log(tokens)
+const bodyParser = require('body-parser')
+const crypto =  require('crypto')
+const deploy = require('./deploy.js')
+
 const app = express()
 
+app.use(bodyParser.json());
 app.post('/deploy', (req, res) => {
-    console.log(JSON.parse(req))
-    if (!req.query || !req.query.token) return res.send(403)
-    if (req.query.token !== tokens.deploy_token) return res.send(404)
-    return res.send('Good work')
+    if (!req.get('X-Hub-Signature')) return res.sendStatus(403)
+    const hmac = crypto.createHmac("sha1", process.env.WEBHOOK_DEPLOY_SECRET || '')
+    const calculatedSignature = "sha1=" + hmac.update(JSON.stringify(req.body)).digest("hex")
+    if (req.get('X-Hub-Signature') !== calculatedSignature) return res.sendStatus(403)
+    res.sendStatus(200)
+    deploy.run(req.body)
+    return
 })
 
 app.listen(8500, () => {
     console.log('Listening on 8500')
 })
-
-// toolbox.routing.getExpressApp().post('/api/pod-transfer', async (req, res) => {
-// +  try {
-// +    await actions.movePods(req.body.currentUser, req.body.targetUserId, req.body.fromPod, req.body.toPod)
-// +    return res.status(200).end()
-// +  } catch (e) {
-// +    if (e.message === 'No permission') return res.status(403).send(e.message)
-// +    toolbox.logger.warn(e.message, 'Error occured during pod transfer')
-// +    return res.status(500).send(e.message)
-// +  }
-// +})
