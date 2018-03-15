689c3f370d4be5e78a621647e197748188389225
YcleptJohn <Ycleptjohn@gmail.com>
2018-03-15 00:46:44 +0000

    Remove ~ from paths

--- src/deploy.js
+++ src/deploy.js
@@ -3,10 +3,10 @@ const cmd = require('../lib/cmd.js')
 
 deploy.run = (payload) => {
     if (payload.ref !== 'refs/heads/master') return console.log('PUSH event was NOT to the master branch')
-    cmd.run('pm2 stop kucc-server', '~/code/kucc')
-    cmd.run('git fetch origin master', '~/code/kucc')
-    cmd.run('git reset --hard origin/master', '~/code/kucc')
-    cmd.run('NODE_ENV=production npm install && npm run build:all', '~/code/kucc')
-    cmd.run('pm2 start src/server.js --name kucc-server', '~/code/kucc')
+    cmd.run('pm2 stop kucc-server', '/kucc')
+    cmd.run('git fetch origin master', '/kucc')
+    cmd.run('git reset --hard origin/master', '/kucc')
+    cmd.run('NODE_ENV=production npm install && npm run build:all', '/kucc')
+    cmd.run('pm2 start src/server.js --name kucc-server', '/kucc')
     console.log('Deployment finished!')
 }
